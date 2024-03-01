6978b9195c505e3dfead443095196f18b7850e70
Haroon Arshad <arshadharoon216@gmail.com>
2024-03-01 23:40:55 +0500

    impact

---
+++ style.js
@@ -0,0 +1,8 @@
+     var navlink = document.getElementById ("navlink");
+
+     function hideMenu(){
+        navlink.style.right = "0";
+     }
+     function showMenu(){
+        navlink.style.right = "-400px";
+     }
